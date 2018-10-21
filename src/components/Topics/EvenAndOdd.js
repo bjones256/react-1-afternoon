331e0699b503b7fae9875c5259fb6a9f3acd8f1a
Brian Jones <BriansMac@Brians-MacBook-Pro.local>
2018-10-21 13:59:58 -0600

    finished even/odds

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -11,21 +11,20 @@ class EvenAndOdd extends Component{
         }
     }
     userInput(e){
-        console.log('user is typing shit in')
         let input = e.target.value
         this.setState({
             userInput : input
         })
-        console.log(input)
+        // console.log(input)
     }
     handleClick(){
-        console.log('user hit the button')
         // create a function here to determine which array and push to proper array
         let {evenArray,oddArray} = this.state
         let evenArr = this.state.evenArray
         let oddArr = this.state.oddArray
+        // convert the string into an array so for each can loop over
         let split = this.state.userInput.split(',')
-        console.log(split)
+        // console.log(split)
         split.forEach( e => e % 2 === 0 ? evenArr.push(e) : oddArr.push(e))
         this.setState({
             evenArray : evenArr,
@@ -34,14 +33,14 @@ class EvenAndOdd extends Component{
        
     }
         render(){
-            console.log(this.state.evenArray)
+            // console.log(this.state.evenArray)
             return(
             <div className="puzzleBox evenAndOddPB">
                 <h4>Evens and Odds</h4>
                 <input className="inputLine" onChange={e => this.userInput(e)}/>
                 <button className="confirmationButton" onClick={()=> this.handleClick()}>Split</button>
-                <span className="resultsBox">Evens: [{this.state.evenArray}]</span>
-                <span className="resultsBox">Odds: [{this.state.oddArray}]</span>
+                <span className="resultsBox">Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className="resultsBox">Odds: {JSON.stringify(this.state.oddArray)}</span>
             </div>        )
         }
     
